fe477062dc735adc30f78c0ae0e167337dc535b0
imgibson <96395432+imgibson@users.noreply.github.com>
2025-10-15 16:22:05 +0900

    Add ArchLinux icons.

--- resource/resource.h
+++ resource/resource.h
@@ -2,14 +2,16 @@
 // Microsoft Visual C++ generated include file.
 // Used by resource.rc
 //
-#define IDI_APP_ICON_RED                101
-#define IDI_APP_ICON_BLUE               102
+#define IDI_QEMU_ICON_RED               101
+#define IDI_QEMU_ICON_BLUE              102
+#define IDI_ARCH_ICON_TANGO             103
+#define IDI_ARCH_ICON_CRYSTAL           104
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
